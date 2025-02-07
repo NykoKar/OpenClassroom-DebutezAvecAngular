07036066652ef95e17f6942e6f5cd827373a9cb1
Nicolas Carrion <ncarrion@nero.com>
2025-02-07 11:32:58 +0100

    add list display to main component

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -9,36 +9,34 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
+    this.faceSnaps = [
+      new FaceSnap(
       'Boubou',
       'A teddy bear',
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       new Date(),
       17
-    );
-
-    this.myOtherSnap = new FaceSnap(
+    ),
+    new FaceSnap(
       'Bibi',
       'A grizzli',
       'https://cdn.pixabay.com/photo/2025/02/02/01/20/grizzly-9375881_1280.jpg',
       new Date(),
       3
-    );
-
-    this.myLastSnap = new FaceSnap(
+    ),
+    new FaceSnap(
       'Deda',
       'A bird',
       'https://cdn.pixabay.com/photo/2025/01/26/20/33/robin-9361610_1280.jpg',
       new Date(),
       10
-    );
+    )
+  ];
 
-    this.myOtherSnap.setLocation('Paris');
+    this.faceSnaps[1].setLocation('Paris');
   }
 
 }
