61cfca7248d79fc4efa6532204c333ec00a3822b
Nicolas Carrion <ncarrion@nero.com>
2025-02-07 16:21:21 +0100

    add routing with routerLink or programmaticaly
    + create menu on header
    + move button style to app scss file

--- snapface/src/app/app.routes.ts
+++ snapface/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: 'facesnaps', component: FaceSnapListComponent },
   { path: '', component: LandingPageComponent },
-  { path: 'facesnaps', component: FaceSnapListComponent }
 ];
--- snapface/src/app/landing-page/landing-page.component.ts
+++ snapface/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  constructor( private router: Router) {}
+
+  displayFaceSnaps() {
+    this.router.navigateByUrl('facesnaps');
+  }
 }
